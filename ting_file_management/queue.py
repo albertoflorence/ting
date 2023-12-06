f29dda1b3836eacd62b3672701bf5d91e05319ba
albertoflorence <alberto.sabatier@gmail.com>
2023-12-06 11:10:33 -0300

    requisito 1 - implemente uma queue

--- ting_file_management/queue.py
+++ ting_file_management/queue.py
@@ -1,18 +1,58 @@
 from ting_file_management.abstract_queue import AbstractQueue
 
 
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+
 class Queue(AbstractQueue):
     def __init__(self):
-        """Inicialize sua estrutura aqui"""
+        self.top = None
+        self.tail = None
+        self.size = 0
 
     def __len__(self):
-        """Aqui irá sua implementação"""
+        return self.size
 
     def enqueue(self, value):
-        """Aqui irá sua implementação"""
+        node = Node(value)
+
+        if self.top is None:
+            self.top = node
+        else:
+            self.tail.next = node
+
+        self.tail = node
+        self.size += 1
+
+        return node.value
 
     def dequeue(self):
-        """Aqui irá sua implementação"""
+        if self.is_empty():
+            raise IndexError(
+                "Não é possível remover elementos de uma fila vazia"
+            )
+
+        value = self.top.value
+        self.top = self.top.next
+        self.size -= 1
+
+        if self.size == 0:
+            self.top = None
+
+        return value
 
     def search(self, index):
-        """Aqui irá sua implementação"""
+        if index < 0 or index >= self.size:
+            raise IndexError("Índice Inválido ou Inexistente")
+
+        node = self.top
+        for i in range(index):
+            node = node.next
+
+        return node.value
+
+    def is_empty(self):
+        return self.size == 0
